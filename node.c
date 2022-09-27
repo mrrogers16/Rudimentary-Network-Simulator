98cf20fbf1f198adfd7824eacdf50df081c73101
mclarke16 <107970544+mclarke16@users.noreply.github.com>
2022-09-27 16:48:18 -0500

    a

--- node.c
+++ node.c
@@ -81,14 +81,14 @@ void build_sim(Log *simList, char buffer[], FILE *sim)
             {
                 printf("Buffer is empty or Columns function failed");
             }
-            else if (columns == 4)
+            if (columns == 4)
             {
                 sscanf(buffer, "%u,%[^,],%u,%u,%u", &simList[i].start_time, simList[i].msg, &simList[i].msg_id, &simList[i].start_node, &simList[i].end_node);
                 printf("Start Time: %u\nMessage: %s\nMessage ID: %u\nStart Node: %u\nEnd Node: %u\n--------\n", simList[i].start_time, simList[i].msg, simList[i].msg_id, simList[i].start_node, simList[i].end_node);
                 i++;
                 break;
             }
-            else if (columns == 2)
+            if (columns == 2)
             {
                 sscanf(buffer, "%u,%s,%u", &simList[i].start_time, simList[i].msg, &simList[i].start_node);
                 printf("Timestamp: %u\nRep: %s\nNode ID:", &simList[i].start_time, &simList[i].msg, &simList[i].start_node);
