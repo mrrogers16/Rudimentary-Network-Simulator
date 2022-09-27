6f566685b55819ece34a2cb3bf508987e41d5812
mclarke16 <107970544+mclarke16@users.noreply.github.com>
2022-09-27 18:41:06 -0500

    Update rns.c

--- rns.c
+++ rns.c
@@ -33,7 +33,7 @@ int main(int argc, char **argv)
             else
             {
                 buildNode(nodeList, buffer, fp);
-                continue;
+                
             }
             break;
         case 's':
